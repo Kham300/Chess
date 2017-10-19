e1a0a5ebe6ba0ba7d468911952a418be78a4035e
Khamid <boogaga3000@gmail.com>
2017-10-19 13:00:05 +0300

    finish creating all pieces

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -3,7 +3,6 @@ package com.chess.engine.pieces;
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
-import com.google.common.collect.ImmutableList;
 
 import java.util.Collection;
 
@@ -11,15 +10,21 @@ public abstract class Piece {//класс шахматная фигура
 
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
-    Piece (final int piecePosition, final Alliance pieceAlliance){
+    Piece(final int piecePosition, final Alliance pieceAlliance){
         this.pieceAlliance = pieceAlliance;
         this.piecePosition = piecePosition;
+        this.isFirstMove = false;
     }
 
     public abstract Collection<Move> calculateLegalMoves(final Board board);
 
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
+    }
+
+    public boolean isFirstMove(){
+        return this.isFirstMove;
     }
 }
